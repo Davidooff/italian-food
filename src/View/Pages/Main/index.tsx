f7a896da98a38e00cbbec5b0e523e72c89548cab
Davidooff <david.ter.18@gmail.com>
2023-05-13 05:44:41 +0300

    first push

---
+++ src/View/Pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../../Sections/Header";
+
+import "./main.scss";
+
+function Main() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+export default Main;
---
+++ src/View/Sections/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>HOME</li>
+        <li>MENU</li>
+        <li>OUR STORY</li>
+        <li>CONTACT US</li>
+      </ul>
+    </header>
+  );
+}
+export default Header;
